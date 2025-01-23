976427d9658e4d5981dbc386d1e9bbe79b14e8b2
ABroadCoder <148570855+ABroadCoder@users.noreply.github.com>
2025-01-23 10:30:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 //HTML Element References
 
+const popupContainer = document.querySelector('.popup-container');
+const closePopupContainer = document.querySelector('.close-popup-container');
+const popupTextContainer = document.querySelector('.popup-text-container');
 const topContainer = document.querySelector('.top-container');
 const timerContainer = document.querySelector('.timer-container');
 const instructionsContainer = document.querySelector('.instructions-container');
@@ -13,11 +16,13 @@ const scoreOverallContainer = document.querySelector(
 );
 const scoreTitleContainer = document.querySelector('.score-data-container');
 const scoreContainer = document.querySelector('.score-container');
+const overlay = document.querySelector('.overlay');
 
 // Sound File References
 
 const correctSound = new Audio('lipsclick.wav');
 const incorrectSound = new Audio('pc-game-ui-error.wav');
+const victorySound = new Audio('succeeded-game-jingle.wav');
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -25,6 +30,7 @@ const incorrectSound = new Audio('pc-game-ui-error.wav');
 
 correctSound.volume = 0.2;
 incorrectSound.volume = 0.2;
+victorySound.volume = 0.2;
 
 // Variable Initializations
 
@@ -69,15 +75,33 @@ const exemptKeys = [
   'ArrowLeft',
   'ArrowRight',
   'Backspace',
+  'Escape',
 ];
 
 let currentLetter = '';
-let currentScore = 0;
+let currentPoints = 0;
+let currentTarget = 16;
 let levelLength = [15, 20, 25, 30, 35];
 let numberCorrect = 0;
 let numberIncorrect = 0;
-let minutes = '2';
-let seconds = '00';
+let firstCorrectKeyPressed = false;
+
+const timerValues = [
+  [0, 30],
+  [1, 0],
+  [1, 30],
+  [2, 0],
+  [2, 30],
+  [3, 0],
+];
+
+const pointsTargets = [30, 60, 90, 120, 150, 180];
+
+const messages = [
+  'Welcome to the game! To start, close this popup.',
+  "You're out of time! Try again!",
+  "Congratulations! You've won the game!",
+];
 
 // Function Definitions
 
@@ -93,47 +117,97 @@ const addBox = function () {
 };
 
 let addPoints = function () {
-  currentScore = currentScore + 5;
+  currentPoints = currentPoints + 5;
 };
 
 let subtractPoints = function () {
-  if (currentScore > 0) {
-    currentScore--;
+  if (currentPoints > 0) {
+    currentPoints--;
   } else {
     return;
   }
 };
 
-const displayScore = function () {
+const displayPoints = function () {
   scoreContainer.innerHTML = '';
-  scoreContainer.insertAdjacentHTML('afterbegin', `${currentScore}`);
+  scoreContainer.insertAdjacentHTML(
+    'afterbegin',
+    `${currentPoints}/${pointsTargets[5]}`
+  );
 };
 
 const correctType = function () {
   addPoints();
   numberCorrect++;
-  displayScore();
+  displayPoints();
   correctSound.play();
 };
 
 const incorrectType = function () {
   subtractPoints();
   numberIncorrect++;
-  displayScore();
+  displayPoints();
   incorrectSound.play();
 };
 
-const updateTimerDisplay = function () {
-  timerContainer.textContent = `${minutes}:${seconds}`;
+const updateTimerDisplay = function (minutes, seconds) {
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(seconds).padStart(2, '0');
+  timerContainer.textContent = `${formattedMinutes}:${formattedSeconds}`;
 };
 
-const updateTimer = function () {};
+const runTimer = function (minutes, seconds) {
+  updateTimerDisplay(minutes, seconds);
+  const timerInterval = setInterval(() => {
+    if (seconds === 0) {
+      if (minutes === 0) {
+        clearInterval(timerInterval);
+        showPopup(messages[1]);
+      } else {
+        minutes--;
+        seconds = 59;
+      }
+    } else {
+      seconds--;
+    }
+    // if (currentPoints >= currentTarget) {
+    //   // beatLevel();
+    //   currentPoints = 0;
+    //   clearInterval(timerInterval);
+    // }
+    updateTimerDisplay(minutes, seconds);
+  }, 1000);
+};
+
+const hidePopup = function () {
+  popupContainer.style.opacity = '0';
+  popupContainer.style.display = 'none';
+  overlay.style.display = 'none';
+};
+
+const showPopup = function (text) {
+  popupContainer.style.opacity = '1';
+  popupContainer.style.display = 'flex';
+  popupTextContainer.textContent = text;
+  overlay.style.display = 'block';
+};
+
+const initializeGame = function () {
+  updateTimerDisplay(0, 0);
+  showPopup(messages[0]);
+};
+
+// const beatLevel = function () {
+//   showPopup(messages[2]);
+//   clearInterval(timerInterval);
+//   victorySound.play();
+// };
 
 // Event Listeners
 
 window.addEventListener('load', function () {
   addBox();
-  displayScore();
+  displayPoints();
 });
 
 window.addEventListener('keyup', function (e) {
@@ -143,7 +217,24 @@ window.addEventListener('keyup', function (e) {
   } else if (e.key.toUpperCase() === currentLetter) {
     addBox();
     correctType();
+    if (firstCorrectKeyPressed === false) {
+      firstCorrectKeyPressed = !firstCorrectKeyPressed;
+      runTimer(...timerValues[1]);
+      console.log(...timerValues[1]);
+    }
   } else {
     incorrectType();
   }
 });
+
+closePopupContainer.addEventListener('click', hidePopup);
+window.addEventListener('keyup', function (e) {
+  if (e.key === 'Escape') {
+    initializeGame();
+    hidePopup();
+  }
+});
+
+// Window Initialization
+
+initializeGame();
